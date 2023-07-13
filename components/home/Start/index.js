79986e6ddabb60c2579cba3b1c4077ec28dfaf7d
Souvik Sarkar <souvik.sarkar@iemlabs.com>
2023-07-12 22:36:52 -0700

    Temporary Signout button added.

--- components/home/Start/index.js
+++ components/home/Start/index.js
@@ -1,22 +1,33 @@
 import { View, StyleSheet } from "react-native"
 import { useRouter } from "expo-router"
+import { getAuth, signOut } from "firebase/auth"
 
 import Button from "../../ui/Button"
+
+const auth = getAuth()
 
 const Start = () => {
     const router = useRouter()
     const startHandler = () => {
         router.push('/modules')
     }
+    const signOutHandler = async () => {
+        await signOut(auth)
+    }
     return (<View style={styles.start}>
-        <Button label={'Start Learning'} onPress={startHandler} dark/>
+        <Button label={'Start Learning'} onPress={startHandler} dark />
+        <Button label={'Signout'} onPress={signOutHandler} style={styles.signout} small />
     </View>)
 }
 
 const styles = StyleSheet.create({
     start: {
         flex: 0.3,
-        paddingTop: 20
+        paddingTop: 20,
+        alignItems: 'center'
+    },
+    signout: {
+        width: '50%'
     }
 })
 
