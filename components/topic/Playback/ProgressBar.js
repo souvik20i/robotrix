fd7621cacc6ae2bb8679d53b9ff9f333777dcab1
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-07-17 14:17:38 +0530

    Progress stored in local storage.

--- components/topic/Playback/ProgressBar.js
+++ components/topic/Playback/ProgressBar.js
@@ -1,5 +1,8 @@
-import { View, Text, TouchableWithoutFeedback, StyleSheet } from "react-native"
+import { useEffect } from "react"
+import { View, Text, TouchableWithoutFeedback, TouchableOpacity, StyleSheet } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
 
+import AsyncStorage from "@react-native-async-storage/async-storage"
 import Slider from "@react-native-community/slider"
 import colors from "../../../public/colors"
 
@@ -7,35 +10,59 @@ const ProgressBar = ({ current, length, onChange }) => {
     const currentTimestamp = convertMillis(current)
     const totalDuration = convertMillis(length)
 
+    useEffect(() => {
+        const setMaxReached = async () => {
+            const stored = await AsyncStorage.getItem('max-reached') || 0;
+            if (current > stored) await AsyncStorage.setItem('max-reached', current.toString())
+        }
+        setMaxReached().catch(err => console.log(err))
+    }, [current])
+
+    const slidingHandler = async (value) => {
+        const stored = await AsyncStorage.getItem('max-reached');
+        if (value < stored) onChange(value)
+    }
+
     return (<TouchableWithoutFeedback>
-        <View style={styles.progressBar}>
-            <Text style={styles.time}>{currentTimestamp}</Text>
-            <Slider
-                style={styles.slider}
-                value={current}
-                minimumValue={0}
-                maximumValue={length}
-                minimumTrackTintColor={colors.bgSecondary}
-                maximumTrackTintColor={colors.bgGrey}
-                thumbTintColor={colors.bgSecondary}
-                onSlidingComplete={onChange}
-            />
-            <Text style={styles.time}>{totalDuration}</Text>
+        <View style={styles.bottomBar}>
+            <View style={styles.progressBar}>
+                <Text style={styles.time}>{currentTimestamp}</Text>
+                <Slider
+                    style={styles.slider}
+                    value={current}
+                    minimumValue={0}
+                    maximumValue={length}
+                    minimumTrackTintColor={colors.bgSecondary}
+                    maximumTrackTintColor={colors.bgGrey}
+                    thumbTintColor={colors.bgSecondary}
+                    onSlidingComplete={slidingHandler}
+                />
+                <Text style={styles.time}>{totalDuration}</Text>
+            </View>
+            <TouchableOpacity>
+                <MaterialIcons name="fullscreen" size={24} color={colors.textLight} />
+            </TouchableOpacity>
         </View>
     </TouchableWithoutFeedback>)
 }
 
 const styles = StyleSheet.create({
+    bottomBar: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
     progressBar: {
         flexDirection: 'row',
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingHorizontal: 5
     },
     time: {
         color: colors.textLight
     },
     slider: {
         height: 10,
-        width: '70%'
+        width: '75%'
     }
 })
 
