6fee81594e761ce794abc692ed5ef50244ea1bf4
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-07-28 18:37:18 +0530

    Authentication hook is fixed.

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     label: {
         textAlign: 'center',
         paddingVertical: 10,
-        paddingHorizontal: 50
+        paddingHorizontal: 35
     }
 })
 
--- hooks/use-auth.js
+++ hooks/use-auth.js
@@ -1,7 +1,12 @@
+import { useState, useEffect } from "react"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
-const useAuth = async () => {
-    const username = await AsyncStorage.getItem('username')
+const useAuth = () => {
+    const [username, setUsername] = useState()
+    useEffect(() => {
+        (async () => setUsername(await AsyncStorage.getItem('username')))()
+    }, [])
+    console.log(username)
     return username
 }
 
