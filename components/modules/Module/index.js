7ac4388614d1e8466fd91db4f49a53581095b154
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-07-28 11:48:19 +0530

    Image is added in Module tab.

--- components/modules/Module/index.js
+++ components/modules/Module/index.js
@@ -1,31 +1,43 @@
-import { TouchableOpacity, Text, StyleSheet } from "react-native"
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native"
 import colors from "../../../public/colors"
 
 const Module = ({ id, name, onPress = () => { } }) => {
     return (<TouchableOpacity style={styles.module} onPress={onPress}>
         <Text style={styles.header}>Module {id}</Text>
+        <Image
+            style={styles.image}
+            source={require('../../../public/module.jpg')}
+            resizeMode='contain'
+        />
         <Text style={styles.title}>{name}</Text>
     </TouchableOpacity>)
 }
 
 const styles = StyleSheet.create({
     module: {
-        backgroundColor: colors.bgSecondary,
+        backgroundColor: colors.bgPrimary,
         justifyContent: 'space-between',
         paddingVertical: 20,
         paddingHorizontal: 10,
         marginVertical: 10,
+        marginHorizontal: 5,
         borderRadius: 10,
-        elevation: 3
+        elevation: 5
     },
     header: {
-        color: colors.textLight,
-        fontWeight: 'bold',
+        color: colors.textDark,
         fontSize: 20,
-        paddingBottom: 15
+        textAlign: 'center'
+    },
+    image: {
+        width: '100%',
+        height: 200,
+        marginVertical: 15,
+        borderRadius: 10
     },
     title: {
-        color: colors.textLight,
+        color: colors.textDark,
+        fontSize: 20
     }
 })
 
