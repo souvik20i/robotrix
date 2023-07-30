9354e81107c58e38b978c8166bb150cb0630c2c7
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-07-30 13:46:15 +0530

    Video duration is added in topic tab.

--- components/modules/Topic/index.js
+++ components/modules/Topic/index.js
@@ -1,14 +1,17 @@
-import { TouchableOpacity, Text, StyleSheet } from "react-native"
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
 import { useRouter, usePathname } from "expo-router"
 import { useDispatch } from "react-redux"
 import { moduleActions } from "../../../store/module-slice"
+import { FontAwesome5 } from '@expo/vector-icons'
 
+import useConversion from "../../../hooks/use-conversion"
 import colors from "../../../public/colors"
 
 const Topic = ({ id, title }) => {
     const router = useRouter()
     const pathname = usePathname()
     const dispatch = useDispatch()
+    const duration = useConversion(Math.floor(Math.random() * 600000))
 
     const changeTopicHandler = () => {
         dispatch(moduleActions.changeCurrentTopic(title))
@@ -17,6 +20,10 @@ const Topic = ({ id, title }) => {
 
     return (<TouchableOpacity style={styles.topic} onPress={changeTopicHandler}>
         <Text style={styles.title}>{title}</Text>
+        <View style={styles.duration}>
+            <FontAwesome5 name="video" size={12} color={colors.textGrey} />
+            <Text style={styles.time}>{duration}</Text>
+        </View>
     </TouchableOpacity >)
 }
 
@@ -32,6 +39,17 @@ const styles = StyleSheet.create({
         color: colors.textDark,
         fontSize: 15,
         lineHeight: 30
+    },
+    duration: {
+        width: 50,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 10
+    },
+    time: {
+        fontSize: 12,
+        color: colors.textGrey
     }
 })
 
